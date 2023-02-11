58db8809e3cffc8c2510d68886ef075ec7ab912e
R4GN4R <jimchr2001@gmail.com>
2023-02-11 20:28:42 +0200

    update
    Co-Authored-By: Stratis Dermanoutsos <stratis.dermanoutsos@gmail.com>
    Co-Authored-By: JohnAthanasopoulos <61366457+John-Athanasopoulos@users.noreply.github.com>

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module LabsPortal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
+    config.action_controller.include_all_helpers = false
 
     # Configuration for the application, engines, and railties goes here.
     #
